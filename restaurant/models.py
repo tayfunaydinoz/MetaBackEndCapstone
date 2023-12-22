ae7513a75e2cc2d832a385e5b8626a73e6dbaa7e
Tayfun AYDINÖZ <tayfun.aydinoz@hotmail.com>
2023-12-22 11:20:01 +0300

    unit test added

--- restaurant/models.py
+++ restaurant/models.py
@@ -6,6 +6,9 @@ class Menu(models.Model):
     price=models.DecimalField(max_digits=10,decimal_places=2)
     inventory=models.IntegerField(default=1)
 
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
+
 
 class Booking(models.Model):
     name=models.CharField(max_length=255)
--- restaurant/tests.py
+++ restaurant/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Menu
 
 # Create your tests here.
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title="IceCream", price=80.22, inventory=100)
+        itemstr=item.get_item()
+        self.assertEqual(itemstr, "IceCream : 80.22")
